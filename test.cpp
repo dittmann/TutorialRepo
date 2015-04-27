18f6cba49ea73fb7604aaf915bf05977aabc6369
Joe Pastika <joe.pastika@gmail.com>
2015-04-26 21:41:55 -0500

    added test.cpp

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+const int N = 100;
+
+int main()
+{
+    bool sieve[N];
+    for(bool& element : sieve) element = true;
+    for(int i = 2; i*i< N; ++i)
+    {
+        if(sieve[i])
+        {
+            for(int j = 2*i; j < N; j += i)
+            {
+                sieve[j] = false;
+            }
+        }
+    }
+
+    for(int i = 2; i < N; ++i)
+    {
+        if(sieve[i]) std::cout << i << std::endl;
+    }
+}
